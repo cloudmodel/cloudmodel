f386a033e723ddc07f349bbdbd1caf75580bb7d4
Sven G. Brönstrup <broenstrup@spom.net>
2015-06-09 00:26:49 +0200

    Fixed redis service

--- app/workers/cloud_model/services/redis_worker.rb
+++ app/workers/cloud_model/services/redis_worker.rb
@@ -6,10 +6,17 @@ module CloudModel
         chroot! @guest.deploy_path, "apt-get install redis-server -y", "Failed to install Redis"
         
         puts "        Write redis config"
-        @host.sftp.file.open(File.expand_path("etc/redis.conf", @guest.deploy_path), 'w') do |f|
-          f.write render("/cloud_model/guest/etc/redis.conf", guest: @guest, model: @model)
+        @host.sftp.file.open(File.expand_path("etc/redis/redis.conf", @guest.deploy_path), 'w') do |f|
+          f.write render("/cloud_model/guest/etc/redis/redis.conf", guest: @guest, model: @model)
         end
       end
+      
+      def auto_start
+        puts "        Add Redis to runlevel default"
+        @host.exec "ln -sf /lib/systemd/system/redis-server.service #{@guest.deploy_path.shellescape}/etc/systemd/system/multi-user.target.wants/"
+      end
+      
     end
   end
 end
+
