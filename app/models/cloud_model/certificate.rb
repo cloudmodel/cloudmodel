d8a9e0fbc64c884b59d91e7f3026a416db9d7f8b
Sven G. Brönstrup <broenstrup@spom.net>
2015-06-17 23:44:58 +0200

    Added some certificate helper functions

--- app/models/cloud_model/certificate.rb
+++ app/models/cloud_model/certificate.rb
@@ -19,5 +19,33 @@ module CloudModel
     def to_s
       name
     end
+    
+    def x509
+      OpenSSL::X509::Certificate.new crt
+    end
+    
+    def pkey
+      OpenSSL::PKey.read key
+    end
+    
+    def valid_from
+      x509.not_before
+    end
+    
+    def valid_thru
+      x509.not_after
+    end
+    
+    def common_name
+      x509.subject.to_a.find{|x| x[0] == "CN"}[1]
+    end
+    
+    def issuer
+      x509.issuer.to_a.find{|x| x[0] == "CN"}[1]
+    end
+    
+    def check_key
+      x509.check_private_key pkey
+    end
   end
 end
