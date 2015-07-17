59dcaae704a2db7b1d3d57aa6cfbfc6704e49158
Sven G. Brönstrup <broenstrup@spom.net>
2015-07-17 16:43:53 +0200

    Use busybox/dropbear instead of OpenSSH

--- app/workers/cloud_model/services/ssh_worker.rb
+++ app/workers/cloud_model/services/ssh_worker.rb
@@ -3,25 +3,29 @@ module CloudModel
     class SshWorker < CloudModel::Services::BaseWorker
       def write_config
         puts "        Install SSH"
-        chroot! @guest.deploy_path, "apt-get install ssh -y", "Failed to install SSH"
+        #chroot! @guest.deploy_path, "apt-get install ssh -y", "Failed to install SSH"
+        chroot! @guest.deploy_path, "apt-get install busybox dropbear -y", "Failed to install BusyBox"
         
+        mkdir_p "#{@guest.deploy_path}/etc/ssh"
         puts "        Write SSH config"
-        @host.sftp.file.open(File.expand_path("etc/ssh/sshd_config", @guest.deploy_path), 'w') do |f|
-          f.write render("/cloud_model/guest/etc/ssh/sshd_config", guest: @guest, model: @model)
-        end
+        render_to_remote "/cloud_model/guest/etc/default/dropbear", "#{@guest.deploy_path}/etc/default/dropbear", 0600, service: @model
+        
+        # @host.sftp.file.open(File.expand_path("etc/ssh/sshd_config", @guest.deploy_path), 'w') do |f|
+        #   f.write render("/cloud_model/guest/etc/ssh/sshd_config", guest: @guest, model: @model)
+        # end
       
         # Copy or Generate sshd key
         ssh_host_key_target = File.expand_path("etc/", @guest.deploy_path)
-        ssh_host_key_source = "/inst/hosts_by_ip/#{@guest.private_address}/etc/ssh"
-      
-      
+        ssh_host_key_source = "/inst/hosts_by_ip/#{@guest.private_address}/etc/dropbear"
+          
         mkdir_p ssh_host_key_source
-        %w(dsa ed25519 rsa).each do |type|
-          key_file = "#{ssh_host_key_source}/ssh_host_#{type}_key"
+        %w(dss ecdsa rsa).each do |type|
+          key_file = "#{ssh_host_key_source}/dropbear_#{type}_host_key"
           begin
             @host.sftp.lstat! key_file
           rescue Net::SFTP::StatusException => e
-            @host.exec! "ssh-keygen -t #{type} -f #{key_file.shellescape} -N ''", 'Failed to generate host keys'
+            #@host.exec! "ssh-keygen -t #{type} -f #{key_file.shellescape} -N ''", 'Failed to generate host keys'
+            @host.exec! "#{@guest.deploy_path}/usr/bin/dropbearkey -t #{type} -f #{key_file.shellescape}", "Failed to generate #{type} host keys"
           end
         end
       
@@ -30,18 +34,14 @@ module CloudModel
         # Copy over client ssh files
         ssh_target = File.expand_path("var/www/.ssh", @guest.deploy_path)
         @host.exec "rm -rf #{ssh_target.shellescape}"
-        #@host.exec! "cp -ra /inst/ssh/client_keys #{ssh_target}", "Failed to copy www client keys"
         mkdir_p ssh_target
         @host.sftp.file.open("#{ssh_target}/authorized_keys", 'w') do |f|
           f.write CloudModel::SshPubKey.all.to_a * "\n"
         end
-  
-        # TODO: Reenable after config for ngix done
-        # chroot! @guest.deploy_path, "chown -R www:www /var/www/.ssh", "Failed to change owner of www client keys to user www (1001)"
       end
     
       def service_name
-        "sshd"
+        "dropbear"
       end
       
       def auto_restart
