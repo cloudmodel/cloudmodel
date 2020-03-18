a3d9e78f2cb0a3f33c3c0d52060f583dec07924c
Sven G. Brönstrup <broenstrup@spom.net>
2020-03-18 16:04:45 +0100

    Removed LVM models

--- spec/factories/cloud_model/volume_group_factory.rb
+++
@@ -1,6 +0,0 @@
-Factory.define :volume_group, class: CloudModel::VolumeGroup do |f|
-  f.name { Faker::Internet.domain_word }
-  f.disk_space { '100 GiB' }
-  f.host { Factory :host}
-  f.disk_device { 'md0' }
-end
