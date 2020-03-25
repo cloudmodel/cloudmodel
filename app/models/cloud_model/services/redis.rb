6729df139894f0311d2725f1d5212e508486f600
Sven G. Brönstrup <broenstrup@spom.net>
2020-03-25 09:35:18 +0100

    Reworked service_status gathering and added passenger info to nginx status

--- app/models/cloud_model/services/redis.rb
+++ app/models/cloud_model/services/redis.rb
@@ -20,6 +20,25 @@ module CloudModel
       def components_needed
         [:redis]
       end
+      
+      def service_status        
+        begin
+          redis = ::Redis.new(host: guest.private_address, port: port, db: 0)
+
+          data = redis.info
+        rescue ::Redis::CannotConnectError => e
+          return {key: :not_reachable, error: "#{e.class}\n\n#{e.to_s}", severity: :critical}
+        rescue Exception => e
+          return {key: :not_reachable, error: "#{e.class}\n\n#{e.to_s}", severity: :warning}
+        end
+
+        # Remove keys containing config details and doubled values
+        %w(redis_version redis_git_sha1 redis_mode config_file mem_allocator redis_build_id os arch_bits multiplexing_api gcc_version process_id run_id used_memory_human used_memory_peak_human ).each do |k|
+          data.delete(k)
+        end
+        
+        data
+      end
             
       def redis_sentinel_master?
         if monitoring_last_check_result
--- app/models/cloud_model/services/tomcat.rb
+++ app/models/cloud_model/services/tomcat.rb
@@ -13,6 +13,67 @@ module CloudModel
         [:java, :tomcat]
       end
             
+      def service_status        
+        data = {}
+        uri = URI("http://#{guest.internal_address}:8080/manager/status?XML=true")
+
+        begin
+          res = nil
+  
+          Net::HTTP.start(uri.host, uri.port,
+            :use_ssl => uri.scheme == 'https', 
+            :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+            req = Net::HTTP::Get.new uri.request_uri
+            req.basic_auth 'mon', 'mon'
+
+            res = http.request req 
+          end
+        rescue Exception => e
+           return {key: :not_reachable, error: "#{e.class}\n\n#{e.to_s}", severity: :critical}
+        end
+  
+        data['http_version'] = res.http_version
+
+        if res.code == '404'
+          return {key: :no_tomcat_status, error: "404: tomcat status not found on server, but server running", severity: :warning}
+        end
+        if res.code == '401'
+          return {key: :tomcat_status_forbidden, error: "401: no privileges to access tomcat status on server", severity: :warning}
+        end
+
+        begin
+          doc = Nokogiri::XML(res.body)
+          doc.xpath('//status/jvm/memory').first.attributes.each do |k,v| 
+            data["memory_#{k}"] = v.to_s.to_i
+          end
+  
+          if data['memory_free'] and data['memory_total'] > 0
+            data['memory_usage'] = (100.0 * (data['memory_total']-data['memory_free'])/data['memory_total'])
+          end
+  
+          connector = doc.xpath('//status/connector[@name=\'"http-nio-8080"\']')
+          if connector.size == 0
+            connector = doc.xpath('//status/connector[@name=\'"http-bio-8080"\']')    
+          end
+          if connector.size > 0
+            connector.xpath('./requestInfo').first.attributes.each do |k,v| 
+              data["request_#{k}"] = v.to_s.to_i
+            end
+            connector.xpath('./threadInfo').first.attributes.each do |k,v| 
+              data["thread_#{k.gsub('Thread', '')}"] = v.to_s.to_i
+            end
+    
+            thread_usage = (100.0 * data['thread_currentCount']/data['thread_maxs'])
+            data['thread_usage'] = thread_usage.round(2)
+          end
+        rescue Exception => e
+          return {key: :parse_result, error: "could not parse status xml: #{e}", severity: :warning}
+        end
+        
+        data
+      end
+
       def heap_size
         "#{guest.memory_size / 1024 / 1024 - 128}m"
       end
