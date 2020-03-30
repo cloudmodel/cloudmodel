fc44632e2fd63723b1a187583f1a6a5f2b781d3f
Sven G. Brönstrup <broenstrup@spom.net>
2020-03-30 11:19:35 +0200

    Updated NetAddr to V2

--- app/models/cloud_model/address.rb
+++ app/models/cloud_model/address.rb
@@ -12,30 +12,24 @@ module CloudModel
     field :gateway, type: String
     field :hostname, type: String
     
-    def hostname
-      self[:hostname] ||= begin
-        Resolv.getname(ip)
-      rescue
-        ip
-      end
-    end
-    
     validates :ip, presence: true
     validates :subnet, presence: true
     
     validate :check_ip_format
    
-    def self.initialize(options)
-      if options.class == String
-        self.from_string options
-      else
-        super
-      end
-    end
+    # def initialize(options = {})
+    #   if options.class == String
+    #     self.from_string options
+    #   else
+    #     super
+    #   end
+    # end
    
     def self.from_str(str)
-      cidr = ::NetAddr::CIDR.create(str)
-      self.new ip: cidr.ip(Short: true), subnet: cidr.bits
+      net = ::NetAddr.parse_net(str)
+      ip = ::NetAddr.parse_ip(str.split(/[\/ ]/).first)
+      
+      self.new ip: ip.to_s, subnet: net.netmask.to_s.gsub(/^\//, '')
     end
    
     def to_s options={}
@@ -46,32 +40,28 @@ module CloudModel
       end
     end
    
-    def cidr
-      if subnet
-        ::NetAddr::CIDR.create("#{ip}/#{subnet}")
-      else
-        ::NetAddr::CIDR.create(ip)
+    def hostname
+      self[:hostname] ||= begin
+        Resolv.getname(ip)
+      rescue
+        ip
       end
     end
-    
+       
     def network
-      cidr.network
+      cidr.network.to_s
     end
     
     def netmask
-      cidr.wildcard_mask
-    end
-    
-    def tinc_subnet
-      "16"
-    end    
-    
-    def tinc_network
-      NetAddr::CIDR.create("#{CloudModel::Host.last.private_network.ip}/#{tinc_subnet}").network
+      if ip_version == 4
+        cidr.netmask.extended
+      else
+        cidr.netmask.to_s
+      end
     end
     
     def broadcast
-      cidr.broadcast if ip_version == 4
+      cidr.nth(cidr.len - 1).to_s if ip_version == 4
     end
     
     def ip_version
@@ -79,26 +69,49 @@ module CloudModel
     end
     
     def range?
-      cidr.ip == cidr.network
+      ip == network
     end
     
     def list_ips
       return [] if ip_version==6 # Don't list ips for IPV6
       if range?
-        cidr.enumerate[1..-2]
+        ips = []
+        (cidr.len - 2).times do |i|
+          ips << cidr.nth(i + 1).to_s
+        end
+        ips
+        #cidr.to_a#.enumerate[1..-2]
       else
         [ip]
       end
     end
+
+    def tinc_subnet
+      16
+    end    
     
+    def tinc_network
+      NetAddr.parse_net("#{CloudModel::Host.last.private_network.ip}/#{tinc_subnet}").network.to_s
+    end
+        
     private
     def check_ip_format
       begin
-        ::NetAddr::CIDR.create("#{ip}/#{subnet}")
+        ::NetAddr.parse_net("#{ip}/#{subnet}")
       rescue Exception => e
         self.errors.add(:subnet, :format, default: e.message)
         return false
       end
     end
+    
+    private
+    def cidr
+      if subnet
+        ::NetAddr.parse_net("#{ip}/#{subnet}")
+      else
+        ::NetAddr.parse_net(ip)
+      end
+    end
+    
   end
 end
