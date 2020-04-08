c5b8d972c9a38f2dbb363432a7c2d2ed58f884af
Sven G. Brönstrup <broenstrup@spom.net>
2020-04-08 11:51:08 +0200

    Fix rendering in workers for Rails 6

--- app/models/cloud_model/workers/base_worker.rb
+++ app/models/cloud_model/workers/base_worker.rb
@@ -24,9 +24,7 @@ module CloudModel
       end
     
       def render template, locals={}
-        av = ActionView::Base.new
-        av.view_paths = ActionController::Base.view_paths
-        av.render(template: template, locals: locals)
+        ActionController::Base.new.render_to_string(template: template, locals: locals)
       end
     
       # render_to_remote template, remote_file, [[perms], locals]
