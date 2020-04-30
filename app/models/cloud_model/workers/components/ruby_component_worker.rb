33f50c6118781828fefbc03ea62218d452bfa4c5
Sven G. Brönstrup <broenstrup@spom.net>
2020-04-30 11:04:59 +0200

    Fix components worker for guest templates; use latest bundler instead of Ubutu's one

--- app/models/cloud_model/workers/components/ruby_component_worker.rb
+++ app/models/cloud_model/workers/components/ruby_component_worker.rb
@@ -5,14 +5,15 @@ module CloudModel
         def build build_path
           # Ruby deps needed for most rails projects
           # TODO: Consider separating them to more Components
-          packages = %w(ruby git bundler)
+          packages = %w(ruby git)
           packages += %w(zlib1g-dev libxml2-dev) # Nokogiri
           packages << 'ruby-bcrypt' # bcrypt      
           packages << 'nodejs' # JS interpreter 
           packages << 'imagemagick' # imagemagick (TODO: needed for some rails projects, make this configurable)
           packages << 'libxml2-utils' # xmllint (TODO: needed for some rails projects, make this configurable)
           packages << 'libxslt-dev' # libxml (TODO: needed for some rails projects, make this configurable)
-          chroot! build_path, "apt-get install #{packages * ' '} -y", "Failed to install packages for deployment of rails app"  
+          chroot! build_path, "apt-get install #{packages * ' '} -y", "Failed to install packages for deployment of rails app"
+          chroot! build_path, "gem install bundler", "Failed to install bundler"
         end
       end
     end
