76c831b8f8fc5e80d8d37878de604c79b28c2f89
Sven G. Brönstrup <broenstrup@spom.net>
2020-05-07 11:17:56 +0200

    Install ruby-dev and bundler v1 on ruby component for backward compatibility and being able to compile gems again

--- app/models/cloud_model/workers/components/ruby_component_worker.rb
+++ app/models/cloud_model/workers/components/ruby_component_worker.rb
@@ -5,7 +5,7 @@ module CloudModel
         def build build_path
           # Ruby deps needed for most rails projects
           # TODO: Consider separating them to more Components
-          packages = %w(ruby git)
+          packages = %w(ruby ruby-dev git)
           packages += %w(zlib1g-dev libxml2-dev) # Nokogiri
           packages << 'ruby-bcrypt' # bcrypt      
           packages << 'nodejs' # JS interpreter 
@@ -13,7 +13,8 @@ module CloudModel
           packages << 'libxml2-utils' # xmllint (TODO: needed for some rails projects, make this configurable)
           packages << 'libxslt-dev' # libxml (TODO: needed for some rails projects, make this configurable)
           chroot! build_path, "apt-get install #{packages * ' '} -y", "Failed to install packages for deployment of rails app"
-          chroot! build_path, "gem install bundler", "Failed to install bundler"
+          chroot! build_path, "gem install bundler", "Failed to install current bundler"
+          chroot! build_path, "gem install bundler -v '~>1.0'", "Failed to install legacy bundler v1"
         end
       end
     end
