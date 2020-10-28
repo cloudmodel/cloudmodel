0f33fb1b2cf53315cf9b27895c4c2a71a2418c01
Sven G. Brönstrup <broenstrup@spom.net>
2020-10-28 18:21:07 +0100

    Fixed wkhtmltopdf component worker test for new method to get it

--- spec/models/cloud_model/workers/components/wkhtmltopdf_component_worker_spec.rb
+++ spec/models/cloud_model/workers/components/wkhtmltopdf_component_worker_spec.rb
@@ -3,18 +3,29 @@
 require 'spec_helper'
 
 describe CloudModel::Workers::Components::WkhtmltopdfComponentWorker do
-  let(:host) {double CloudModel::Host}
+  let(:host) {double CloudModel::Host, arch: '6502'}
   subject {CloudModel::Workers::Components::WkhtmltopdfComponentWorker.new host}
 
   it { expect(subject).to be_a CloudModel::Workers::Components::BaseComponentWorker }
 
   describe 'build' do
+    let(:uri) { double URI, read: '<a href="https://github.com/wkhtmltopdf/packaging/releases/download/0.42.23/wkhtmltox_0.42.23.quick_6502.deb">'}
+
     before do
       allow(subject).to receive :chroot!
+      allow(URI).to receive(:parse).and_return uri
+      allow(CloudModel.config).to receive(:ubuntu_short_name).and_return 'quick'
     end
 
-    it 'should apt-get wkhtmltopdf' do
-      expect(subject).to receive(:chroot!).with('/tmp/build', 'apt-get install wkhtmltopdf -y', 'Failed to install packages for wkhtmltopdf')
+    it 'should apt-get wkhtmltopdf dependencies' do
+      expect(subject).to receive(:chroot!).with('/tmp/build', 'apt-get install xfonts-75dpi xfonts-base fontconfig libjpeg-turbo8 libxrender1 -y', 'Failed to install packages for wkhtmltopdf dependencies')
+
+      subject.build '/tmp/build'
+
+    end
+
+    it 'should install wkhtmltopdf' do
+      expect(subject).to receive(:chroot!).with("/tmp/build", "wget -q https://github.com/wkhtmltopdf/packaging/releases/download/0.42.23/wkhtmltox_0.42.23.quick_6502.deb && dpkg -i wkhtmltox_0.42.23.quick_6502.deb && apt -f install", "Failed to install packages for wkhtmltopdf")
 
       subject.build '/tmp/build'
     end
