aaaf2a804810e238d1772bfff743985ca3bdf061
Sven G. Brönstrup <broenstrup@spom.net>
2020-11-09 16:18:09 +0100

    Monitor MongoDB Replication sets, too

--- app/models/cloud_model/monitoring/mongodb_replication_set_checks.rb
+++ app/models/cloud_model/monitoring/mongodb_replication_set_checks.rb
@@ -1,12 +1,35 @@
 module CloudModel
   module Monitoring
     class MongodbReplicationSetChecks < CloudModel::Monitoring::BaseChecks
-      def acquire_data
+      def line_prefix
+        "[_Mongo Repl_] #{super}"
+      end
 
+      def acquire_data
+        @subject.status service_id_only: true
       end
 
       def check
+        if data[:members] and data[:members].size > 0
+          do_check :set_health, 'Set Health', {critical: not(data['ok'] == 1.0)}, message: "Set not healthy"
 
+          member_health = data[:members].select{|m| m["stateStr"] != "ARBITER"}.map{|m| m['health']}
+          average = member_health.sum / member_health.size.to_f
+
+          majority_healthy = average > 0.5
+          healthy_services = data[:members].select{|m| m["health"] == 1.0}
+          all_healthy = healthy_services.size == data[:members].size
+
+          message = if not majority_healthy
+            "Majority of operational members not healthy"
+          else
+            "Some Members or Arbiters are not healty"
+          end
+
+          do_check :member_health, 'Members Health', {critical: not(majority_healthy), warning: not(all_healthy)}, message: message
+
+          return (data['ok'] == 1.0 and all_healthy)
+        end
       end
     end
   end
--- spec/models/cloud_model/monitoring/mongodb_replication_set_checks_spec.rb
+++ spec/models/cloud_model/monitoring/mongodb_replication_set_checks_spec.rb
@@ -9,14 +9,45 @@ describe CloudModel::Monitoring::MongodbReplicationSetChecks do
   it { expect(subject).to be_a CloudModel::Monitoring::BaseChecks }
 
   describe 'acquire_data' do
-    it 'should be nil for now' do
-      expect(subject.acquire_data).to eq nil
+    it 'should get set status' do
+      allow(mongodb_replication_set).to receive(:status).and_return 'ok' => 1.0
+      expect(subject.acquire_data).to eq 'ok' => 1.0
     end
   end
 
   describe 'check' do
-    it 'should be nil for now' do
+    it 'should be nil for no members' do
+      allow(subject).to receive(:data).and_return 'ok' => 0.0
       expect(subject.check).to eq nil
     end
+
+    it 'should be true for all members and set healthy' do
+      expect(subject).to receive(:do_check).with(:set_health, 'Set Health', {critical: false}, message: "Set not healthy")
+      expect(subject).to receive(:do_check).with(:member_health, "Members Health", {critical: false, warning: false}, {:message=>"Some Members or Arbiters are not healty"})
+      allow(subject).to receive(:data).and_return 'ok' => 1.0, members: [{'health' => 1.0, 'stateStr' => 'PRIMARY'}]
+      expect(subject.check).to eq true
+    end
+
+    it 'should be false for all members healthy and set not healthy' do
+      expect(subject).to receive(:do_check).with(:set_health, 'Set Health', {critical: true}, message: "Set not healthy")
+      expect(subject).to receive(:do_check).with(:member_health, "Members Health", {critical: false, warning: false}, {:message=>"Some Members or Arbiters are not healty"})
+      allow(subject).to receive(:data).and_return 'ok' => 0.0, members: [{'health' => 1.0, 'stateStr' => 'PRIMARY'}]
+      expect(subject.check).to eq false
+    end
+
+    it 'should be false for some members not healthy and set healthy' do
+      expect(subject).to receive(:do_check).with(:set_health, 'Set Health', {critical: false}, message: "Set not healthy")
+      expect(subject).to receive(:do_check).with(:member_health, "Members Health", {critical: false, warning: true}, {:message=>"Some Members or Arbiters are not healty"})
+      allow(subject).to receive(:data).and_return 'ok' => 1.0, members: [{'health' => 1.0, 'stateStr' => 'PRIMARY'},{'health' => 1.0, 'stateStr' => 'SECONDARY'}, {'health' => 0.0, 'stateStr' => 'error'}]
+      expect(subject.check).to eq false
+    end
+
+    it 'should be false for majority of members not healthy and set healthy' do
+      expect(subject).to receive(:do_check).with(:set_health, 'Set Health', {critical: false}, message: "Set not healthy")
+      expect(subject).to receive(:do_check).with(:member_health, "Members Health", {critical: true, warning: true}, {:message=>"Majority of operational members not healthy"})
+      allow(subject).to receive(:data).and_return 'ok' => 1.0, members: [{'health' => 1.0, 'stateStr' => 'PRIMARY'},{'health' => 0.0, 'stateStr' => 'error'}, {'health' => 0.0, 'stateStr' => 'error'}]
+      expect(subject.check).to eq false
+    end
+
   end
 end
