548b993b5a73a57d6fccb679c9b4d30ab7d41202
Sven G. Brönstrup <broenstrup@spom.net>
2021-07-06 18:29:49 +0200

    Improved monitoring

--- app/models/cloud_model/monitoring/services/tomcat_checks.rb
+++ app/models/cloud_model/monitoring/services/tomcat_checks.rb
@@ -1,23 +1,23 @@
 module CloudModel
   module Monitoring
     module Services
-      class TomcatChecks < CloudModel::Monitoring::Services::BaseChecks      
+      class TomcatChecks < CloudModel::Monitoring::Services::BaseChecks
         def check
           do_check_for_errors_on data, {
             not_reachable: 'service reachable',
-            no_tomcat_status: 'status available', 
-            tomcat_status_forbidden: 'status forbidden', 
+            no_tomcat_status: 'status available',
+            tomcat_status_forbidden: 'status forbidden',
             parse_result: 'parse status'
           }
-                
+
           do_check_value :mem_usage, data['memory_usage'], {
-            warning: 80,
-            critical: 90
+            critical: 90,
+            warning: 80
           }
-        
+
           do_check_value :thread_usage, data['thread_usage'], {
-            warning: 80,
-            critical: 90
+            critical: 90,
+            warning: 80
           }
         end
       end
