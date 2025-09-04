64e5ea3a40fc0a9062abb629abf3624203707d65
starpeak <broenstrup@spom.net>
2025-09-04 15:37:47 +0200

    Many small fixes

--- app/models/cloud_model/workers/solr_image_worker.rb
+++ app/models/cloud_model/workers/solr_image_worker.rb
@@ -1,12 +1,12 @@
 module CloudModel
   module Workers
     class SolrImageWorker < BaseWorker
-    
+
       def initialize(solr_image)
         @solr_image = solr_image
       end
-    
-      def checkout_git        
+
+      def checkout_git
         unless File.directory?(@solr_image.build_path)
           unless system "mkdir -p #{@solr_image.build_path.shellescape}"
             raise "Could not make checkout directory '#{@solr_image.build_path}'"
@@ -22,67 +22,75 @@ module CloudModel
             return false
           end
         end
-      
+
         git_script = "cd #{@solr_image.build_path.shellescape} && "
         git_script += "git checkout #{@solr_image.git_branch.shellescape} &&"
         git_script += "git pull &&"
         git_script += "git submodule init &&"
         git_script += "git submodule update"
-      
-        begin 
+
+        begin
           run_step "Pulling", git_script
         rescue CloudModel::ExecutionException => e
           CloudModel.log_exception e
           @solr_image.update_attributes build_state: :failed, build_last_issue: "Unable to checkout branch '#{@solr_image.git_branch}' on repository '#{@solr_image.git_repo}'."
           return false
         end
-      
+
         begin
           @solr_image.update_attribute :git_commit, run_step("Get Git Commit", "cd #{@solr_image.build_path} && git log | head -1 | sed s/'commit '//")
         rescue Exception => e
           CloudModel.log_exception e
           @solr_image.update_attribute :git_commit,  "failed to get commit hash"
         end
-            
+
         return true
       end
-        
+
       def get_solr
         @solr_version = File.read("#{@solr_image.build_path}/SOLR_VERSION").strip
         solr_mirror = CloudModel::SolrMirror.find_or_create_by(version: @solr_version)
-      end    
-      
+      end
+
       def package_build
+
+        solr_config = if File.exists?("#{@solr_image.build_path.shellescape}/solr-config")
+          "solr-config"
+        else
+          "solr"
+        end
+
+
         begin
-          run_step "Packaging", "/bin/tar -cpjf #{@solr_image.build_path.shellescape}-building.tar.bz2 --directory #{@solr_image.build_path.shellescape}/solr --exclude={'.git','./.gitignore','./.gitmodules','./tmp/**/*','./log/**/*','./spec','./features','.rspec','.gitkeep','./bundle/#{Bundler.ruby_scope}/cache','.bundle/#{Bundler.ruby_scope}/doc'} ."
+          run_step "Packaging", "tar cpjf #{@solr_image.build_path.shellescape}-building.tar.bz2 --directory #{@solr_image.build_path.shellescape}/#{solr_config} --exclude={'.git','./.gitignore','./.gitmodules','./tmp/**/*','./log/**/*','./spec','./features','.rspec','.gitkeep','./bundle/#{Bundler.ruby_scope}/cache','.bundle/#{Bundler.ruby_scope}/doc'} ."
         rescue CloudModel::ExecutionException => e
           CloudModel.log_exception e
-          @solr_image.update_attributes build_state: :failed, build_last_issue: 'Unable to package image.'      
+          @solr_image.update_attributes build_state: :failed, build_last_issue: 'Unable to package image.'
           return false
         end
         system "mv #{@solr_image.build_path.shellescape}-building.tar.bz2 #{@solr_image.build_path.shellescape}.tar.bz2"
-      
+
         return true
       end
-    
+
       def build options = {}
         return false unless @solr_image.build_state == :pending or options[:force]
-        @solr_image.update_attributes build_state: :running, build_last_issue: nil    
-       
+        @solr_image.update_attributes build_state: :running, build_last_issue: nil
+
         if options[:clean]
           system "rm -rf #{@solr_image.build_path.shellescape}"
         end
-      
-        begin 
+
+        begin
           @solr_image.update_attributes build_state: :checking_out
           unless checkout_git
             return false
           end
-        
+
           unless get_solr
             return false
           end
-      
+
           @solr_image.update_attributes build_state: :packaging
           unless package_build
             return false
@@ -96,25 +104,25 @@ module CloudModel
         rescue Exception => e
           CloudModel.log_exception e
           @solr_image.update_attributes build_state: :failed, build_last_issue: "#{e}"
-          return false     
+          return false
         end
-      
+
         @solr_image.update_attributes build_state: :finished
-  
+
         return true
       end
-        
-      def run_step step, command      
+
+      def run_step step, command
         Rails.logger.debug "### #{step}: #{command}"
         c_out = `#{command}`
         unless $?.success?
           Rails.logger.error "Error running command:\n  #{command}\n  #{$?}\n#{c_out.lines.map{|l| "    #{l}"} * ""}\n#----"
-        
+
           raise ExecutionException.new command, "#{step} failed (#{$?})", c_out
         end
         c_out
       end
-    
+
     end
   end
 end
