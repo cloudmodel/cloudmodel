f085e8133788cbd8a6d9e7eb2e42008fadfd293e
starpeak <broenstrup@spom.net>
2026-06-19 15:59:42 +0200

    Add rake_mode (timer/restart/single) to Services::Rake
    Extends the Rake service model with three execution modes:
    - timer: existing behavior (systemd timer triggers one-shot task)
    - restart: long-lived daemon with Restart=always + SIGTERM graceful shutdown
    - single: runs once on boot, no timer, no auto-restart
    
    Restart and single modes optionally install a systemd path unit
    that watches restart.txt and restarts the service on deploy.

--- app/models/cloud_model/workers/services/rake_worker.rb
+++ app/models/cloud_model/workers/services/rake_worker.rb
@@ -1,23 +1,45 @@
 module CloudModel
   module Workers
     module Services
-      # Worker that installs a scheduled Rake task service inside a guest container.
-      #
-      # Writes a named systemd timer (`rake-<task>.timer`) and service unit
-      # (`rake-<task>.service`) based on the {CloudModel::Services::Rake} model's
-      # timer configuration, then enables the timer in `timers.target.wants`.
       class RakeWorker < CloudModel::Workers::Services::BaseWorker
         def write_config
         end
 
         def auto_start
+          render_to_remote "/cloud_model/guest/etc/systemd/system/rake.service", "#{@guest.deploy_path}/etc/systemd/system/rake-#{@model.rake_task}.service", 644, service: @model
+
+          case @model.rake_mode
+          when 'restart', 'single'
+            auto_start_service
+          else
+            auto_start_timer
+          end
+        end
+
+        private
+
+        def auto_start_service
+          comment_sub_step "Add Rake service to runlevel default"
+          mkdir_p "#{@guest.deploy_path}/etc/systemd/system/multi-user.target.wants"
+          @host.exec "ln -sf /etc/systemd/system/rake-#{@model.rake_task}.service #{@guest.deploy_path.shellescape}/etc/systemd/system/multi-user.target.wants/"
+
+          if @model.rake_restart_on_touch
+            render_to_remote "/cloud_model/guest/etc/systemd/system/rake_restart.path", "#{@guest.deploy_path}/etc/systemd/system/rake-restart-#{@model.rake_task}.path", 644, service: @model
+            render_to_remote "/cloud_model/guest/etc/systemd/system/rake_restart.service", "#{@guest.deploy_path}/etc/systemd/system/rake-restart-#{@model.rake_task}.service", 644, service: @model
+            @host.exec "ln -sf /etc/systemd/system/rake-restart-#{@model.rake_task}.path #{@guest.deploy_path.shellescape}/etc/systemd/system/multi-user.target.wants/"
+            @host.exec "chown -R 100000:100000 #{@guest.deploy_path}/etc/systemd/system/rake-restart-#{@model.rake_task}.path #{@guest.deploy_path}/etc/systemd/system/rake-restart-#{@model.rake_task}.service"
+          end
+
+          @host.exec "chown -R 100000:100000 #{@guest.deploy_path}/etc/systemd/system/rake-#{@model.rake_task}.service #{@guest.deploy_path.shellescape}/etc/systemd/system/multi-user.target.wants/rake-#{@model.rake_task}.service"
+        end
+
+        def auto_start_timer
           comment_sub_step "Add Rake timer to runlevel default"
           mkdir_p "#{@guest.deploy_path}/etc/systemd/system/timers.target.wants"
           render_to_remote "/cloud_model/guest/etc/systemd/system/rake.timer", "#{@guest.deploy_path}/etc/systemd/system/rake-#{@model.rake_task}.timer", 644, service: @model
-          render_to_remote "/cloud_model/guest/etc/systemd/system/rake.service", "#{@guest.deploy_path}/etc/systemd/system/rake-#{@model.rake_task}.service", 644, service: @model
           @host.exec "ln -sf /etc/systemd/system/rake-#{@model.rake_task}.timer #{@guest.deploy_path.shellescape}/etc/systemd/system/timers.target.wants/"
           @host.exec "chown -R 100000:100000 #{@guest.deploy_path}/etc/systemd/system/rake-#{@model.rake_task}.service #{@guest.deploy_path}/etc/systemd/system/rake-#{@model.rake_task}.timer #{@guest.deploy_path.shellescape}/etc/systemd/system/timers.target.wants/rake-#{@model.rake_task}.timer"
-         end
+        end
       end
     end
   end
